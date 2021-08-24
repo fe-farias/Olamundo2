c781bb11d006aaa72c98701db4eb40c0e2bfa572
Felipe Farias <fefarias97@gmail.com>
2021-08-24 13:16:38 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olamundo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Felipe!");
+        }
+    }
+}
